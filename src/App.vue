93a6604a260c346e3c4b2273a01adb3f24935298
Diego Schell Fernandes <diego.schell.f@gmail.com>
2017-04-17 17:28:22 -0300

    Estrutura Vue frontend

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <list></list>
+  </div>
+</template>
+
+<script>
+import List from './components/List'
+
+export default {
+  name: 'app',
+  components: {
+    List
+  }
+}
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: #333;
+    color: #fff;
+  }
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
